34b3a233ecf586abe3469beac0f721e7b0f1471b
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-10-02 15:40:39 -0400

    Updates

---
+++ precache-manifest.ad748d4fbc6ab927b720826b1147786f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585499bf28be56dd0128c88795bb86e1",
+    "url": "/react-clickygame/index.html"
+  },
+  {
+    "revision": "81e06a877fbd06375d0d",
+    "url": "/react-clickygame/static/css/main.863a5107.chunk.css"
+  },
+  {
+    "revision": "9c6a04ef7d7f1ba871b3",
+    "url": "/react-clickygame/static/js/2.ad8f64ec.chunk.js"
+  },
+  {
+    "revision": "81e06a877fbd06375d0d",
+    "url": "/react-clickygame/static/js/main.909ece40.chunk.js"
+  },
+  {
+    "revision": "15734df8b5c83919e40f",
+    "url": "/react-clickygame/static/js/runtime-main.b364d93b.js"
+  },
+  {
+    "revision": "4d5e65a0c53ec380d408bfde1d8ff703",
+    "url": "/react-clickygame/static/media/beth.4d5e65a0.png"
+  },
+  {
+    "revision": "8eb2f8e5a9f6882bad1af814075a3433",
+    "url": "/react-clickygame/static/media/bg.8eb2f8e5.jpg"
+  },
+  {
+    "revision": "612ba8d4e89b5cbe07b279808234f834",
+    "url": "/react-clickygame/static/media/birdperson.612ba8d4.png"
+  },
+  {
+    "revision": "0657e1f4c5b5eb7d25b4bd18811256a3",
+    "url": "/react-clickygame/static/media/evilmorty.0657e1f4.png"
+  },
+  {
+    "revision": "978b38471c32626814803817d75f4ab8",
+    "url": "/react-clickygame/static/media/gianthead.978b3847.png"
+  },
+  {
+    "revision": "3774b39dcbda08977c789663f6049c70",
+    "url": "/react-clickygame/static/media/goldenford.3774b39d.png"
+  },
+  {
+    "revision": "af8e8a84f14297f7c14073059a4b9246",
+    "url": "/react-clickygame/static/media/jerry.af8e8a84.png"
+  },
+  {
+    "revision": "a9644830b29212e0af6fb0a353c1c46d",
+    "url": "/react-clickygame/static/media/jessica.a9644830.png"
+  },
+  {
+    "revision": "9667b7eeed7cbde4439b6b6bba4a040f",
+    "url": "/react-clickygame/static/media/meeseeks.9667b7ee.png"
+  },
+  {
+    "revision": "244deeda3690049cebc0046457a6e1cf",
+    "url": "/react-clickygame/static/media/morty.244deeda.png"
+  },
+  {
+    "revision": "d61b2d5bd91638427b6604f6f7dcf1dc",
+    "url": "/react-clickygame/static/media/mr.d61b2d5b.png"
+  },
+  {
+    "revision": "06b6584198e9fce2075bc1a4a9bfae92",
+    "url": "/react-clickygame/static/media/rick.06b65841.png"
+  },
+  {
+    "revision": "afdbb7bfad6a6acefaafbf9fb072f0cb",
+    "url": "/react-clickygame/static/media/summer.afdbb7bf.png"
+  }
+]);
