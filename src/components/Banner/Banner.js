6630ef8756bea30c0d09289a9ef7f2a2126949e5
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-10-02 13:14:37 -0400

    created banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => (
+    <div className="banner">
+        <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+        <h1 class="display-4">Rick and Morty Memory Game</h1>
+        <p class="lead">Instructions: Click on a Rick and Morty character once. Remember who you clicked because if you click on them more than once, you'll lose!</p>
+  </div>
+</div>
+  </div>
+);
+
+export default Banner;
